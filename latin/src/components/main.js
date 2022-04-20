958507015c0e81ca03f764487d79d3003773ea14
Abhishek Kakolla <abhishekkakolla1@gmail.com>
2022-04-20 02:50:37 -0400

    added image

--- latin/src/components/main.js
+++ latin/src/components/main.js
@@ -1,16 +1,21 @@
 import React from 'react';
-import state from '../App.js'
 
 const SiteInfo = () => {
 
     return (
-        <div style={{display: 'flex', flexWrap: ''}}>
+        <div style={{display: 'flex', flexWrap: 'wrap', alignItems: 'center', justifyContent: 'space-evenly'}}>
+            <div style={{marginTop: '-22rem'}}>
             <ul style={{listStyleType: 'none', lineHeight: '50px', fontSize: '25px'}}>
             <li>Abhishek Kakolla</li> 
             <li>Ms. Rogow</li>
             <li>LVLBD1-01</li>
             <li>23 April 2022</li>
             </ul>
+            </div>
+            
+            <div style={{display: 'flex', flexWrap: 'wrap', alignItems: 'center', justifyContent: 'space-evenly'}}>
+            <img  style={{width: '800px', position: 'relative'}} src="https://www.ancienthistorylists.com/wp-content/uploads/2014/08/animal-sacrifice-in-ancient-rome.jpg"></img>
+            </div>
         </div>
     )
 }
